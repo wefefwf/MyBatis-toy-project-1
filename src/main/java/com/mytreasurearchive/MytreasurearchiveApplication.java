9750dd79f38930536e01602637d116e85287f8b2
= <=>
2026-02-13 17:15:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/mytreasurearchive/MytreasurearchiveApplication.java
@@ -0,0 +1,13 @@
+package com.mytreasurearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytreasurearchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytreasurearchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytreasurearchive/MytreasurearchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytreasurearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytreasurearchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
